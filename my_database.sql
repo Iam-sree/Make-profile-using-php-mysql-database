ef761db7c9f62988c383114db3e9481668cb44cd
Iam-sree <iamsreeganesh87@gmail.com>
2024-10-18 20:36:03 +0530

    Update my_database.sql

--- my_database.sql
+++ my_database.sql
@@ -5,6 +5,6 @@ USE myDatabase;
 CREATE TABLE myTable (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100),
-    password VARCHAR((20)
+    password VARCHAR((20),
     image LONGBLOB
 );
